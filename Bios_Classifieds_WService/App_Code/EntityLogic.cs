03e98201ac9f230e545567a79739f26d3c6f9475
Lucas Hernandez <matiasoppc@gmail.com>
2020-10-23 14:25:58 -0300

    Entity Logic class Created

---
+++ Bios_Classifieds_WService/App_Code/EntityLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Activities.Validation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Model;
+using System.Data;
+using System.Data.SqlClient;
+
+
+/// <summary>
+/// Brinda los servicios del modelo del Entity Framework
+/// </summary>
+public class EntityLogic
+{
+    //Constructor
+    public EntityLogic(){}
+
+    #region Entity Framework
+    //Declaracion del contexto del Entity
+    private static Model.Entities _context = null;
+    //Singleton del context
+    public static Model.Entities Context
+    {
+        get
+        {
+            if (_context == null)
+            {
+                _context = new Entities();
+                _context.Configuration.ProxyCreationEnabled = false;
+            }
+            return _context;
+        }
+    }
+    #endregion
+
+    //OPERACIONES
+
+
+}
